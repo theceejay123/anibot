8d03733fc8b41d375aac2039cbd7e55312802883
Ceejay Pimentel <ceejaypimentel@hotmail.ca>
2020-08-26 10:18:09 -0500

    Adds new shorten command for shortening links in the chat

---
+++ commands/_shorten.js
@@ -0,0 +1,29 @@
+const colors = require("../util/colors");
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "shorten",
+  aliases: ["s", "sh", "short"],
+  description: "Shortens a link.",
+  execute(bot, msg, args) {
+    msg.channel.send("Generating Link...").then((bot_msg) => {
+      const url = `https://sh.onpaper.ca/link`;
+      fetch(url, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          url: args,
+        }),
+      }).then(async (response) => {
+        const jsonData = await response.json();
+        msg.channel.send(jsonData.link);
+        msg.delete();
+        bot_msg.delete();
+      });
+    });
+  },
+};
