e8e38af7f264e024ea835281332d9ad9a96d257c
Ceejay Pimentel <ceejaypimentel@hotmail.ca>
2020-08-31 19:22:38 -0500

    Adds fixes on deletion of bot

--- util/play.js
+++ util/play.js
@@ -7,14 +7,15 @@ module.exports = {
     if (!song) {
       serverQueue.voiceChannel.leave();
       bot.queue.delete(guild.id);
-      return msg.channel.send("There is no song currently in the queue.");
+      return serverQueue.textChannel.send(
+        "There is no song currently in the queue."
+      );
     }
 
     const dispatcher = serverQueue.connection
       .playStream(ytdl(song.url), { filter: "audioonly" })
       .on("end", () => {
         serverQueue.songs.shift();
-        console.log(serverQueue.songs);
         module.exports.play(guild, serverQueue.songs[0], serverQueue, bot);
       })
       .on("error", () => console.error(error));
